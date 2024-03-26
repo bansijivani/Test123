8f653d85228dfa5c2ae18c38ec4533232e531494
Bansi <67221057+bansijivani@users.noreply.github.com>
2024-03-26 11:09:00 +0100

    Create bing-1.c

---
+++ bing-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *ptr = NULL;
+    int buffer[10];
+
+    printf("%d\n", *ptr);  // CWE-476: NULL Pointer Dereference
+
+    for(int i=0; i<=10; i++) {
+        buffer[i] = i;  // CWE-119: Buffer Overflow
+    }
+
+    return 0;
+}
+
