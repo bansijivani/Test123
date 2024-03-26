c998e8f4b221aad1dd77a470692c415d35c4e58e
Bansi <67221057+bansijivani@users.noreply.github.com>
2024-03-26 11:10:06 +0100

    Update bing-1.c

--- bing-1.c
+++ bing-1.c
@@ -5,7 +5,7 @@ int main() {
     int *ptr = NULL;
     int buffer[10];
 
-    printf("%d\n", *ptr);  // CWE-476: NULL Pointer Dereference
+    printf("Value: %d\n", *ptr);  // CWE-476: NULL Pointer Dereference
 
     for(int i=0; i<=10; i++) {
         buffer[i] = i;  // CWE-119: Buffer Overflow
